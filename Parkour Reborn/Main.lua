628792473e0b5798d7f30d0b845e43ccdb6595d6
theonlylegacy <159341819+theonlylegacy@users.noreply.github.com>
2024-03-20 13:14:35 +0100

    Update Main.lua

--- Parkour Reborn/Main.lua
+++ Parkour Reborn/Main.lua
@@ -3,8 +3,8 @@
 
 Features include:
 
-Gear modding:
-Grappler pull force, max distance, speed, infinite uses, enable swing on all objects
-Character infinite respawn health, walkspeed, jumpheight, infinite wallboosts, super slide (inspired from the og parkour scripts), play fidget animations (the game devs dont know how characteradded works)
+
+Grappler: pull force, max distance, speed, infinite uses, enable swing on all objects
+Character: infinite respawn health, walkspeed, jumpheight, infinite wallboosts, super slide (inspired from the og parkour scripts), play fidget animations (the game devs dont know how characteradded works)
 more to come..
 ]]
