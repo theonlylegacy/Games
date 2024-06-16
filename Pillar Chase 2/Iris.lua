25965dd293b28ffeea338c96b20becde93bb905b
Sero <159341819+theonlylegacy@users.noreply.github.com>
2024-06-16 19:38:52 +0200

    Update Iris.lua

--- Pillar Chase 2/Iris.lua
+++ Pillar Chase 2/Iris.lua
@@ -1,1 +1,30 @@
 --not yet (still a work in progress since solara is what it is)
+--[[
+Usage / Feature list:
+
+Infinite Stamina: Your stamina never drains when running or using it in any way
+
+Infinite Range: Allows infinite interaction range for all objectives within the game
+
+Instant Interact: Allows you to instantly interact with any proximityprompt within the game (includes objectives)
+
+Instant Kill: Automatically kills near players when you are on the monster team
+
+No Stun: Blocks any form of stun (includes the dance meter)
+
+No Grab: Blocks you from being grabbed (never seen it work but its supposed to do that anyway)
+
+Always Earn: Blocks AFK earning mode (forces the game to give you earnings even when you are standing still)
+
+Automatic Earn: Forces max earnings within the game instantly if you are a survivor (55 coins per round)
+
+Automatic Heal: Instantly uses up any medkit that you are carrying if your health isn't full
+
+Highlight Monsters: Highlights all monsters if you are a survivor and highlights the other monster if you are a monster and the game mode is set to double trouble
+
+Highlight Objectives: Highlights all objectives if you are a survivor but highlights all the players if you are a monster
+
+Force Vote: Adds 20 votes to selected game mode per click
+
+Unload: Unloads cheat (the ui doesnt fully unload because the guy that made it doesnt know what he's doing)
+]]
