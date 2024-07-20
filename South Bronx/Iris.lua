3eb1a5e5f70c20de5ef54d5297a808b7f4d5e422
Sero <159341819+theonlylegacy@users.noreply.github.com>
2024-07-20 17:37:03 +0200

    Create Iris.lua

---
+++ South Bronx/Iris.lua
@@ -0,0 +1,28 @@
+--WIP
+--[[
+Silent Aim:
+   Enabled
+   Wallbang
+   FriendCheck
+   Limb {"Head", "UpperTorso", "LowerTorso"}
+   Field (Min: 0, Max: 500)
+
+Gun:
+   Infinite Ammo
+   Auto Reload
+   Auto Slide, No Jam
+
+Spawn:
+   Enabled
+   Location {"Dealership", "Apartments", "Firearms"}
+
+Entity:
+   Walk Speed
+   
+PlayerSelect:
+   View
+   Players {:GetPlayers()}
+
+Internal:
+   Unload
+]]
