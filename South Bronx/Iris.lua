0d204cb1ef75cf853df9786c8a8c4bba9f04439c
Sero <159341819+theonlylegacy@users.noreply.github.com>
2024-08-03 19:05:28 +0200

    Update Iris.lua

--- South Bronx/Iris.lua
+++ South Bronx/Iris.lua
@@ -1,28 +1,3 @@
---WIP
---[[
-Silent Aim:
-   Enabled
-   Wallbang
-   FriendCheck
-   Limb {"Head", "UpperTorso", "LowerTorso"}
-   Field (Min: 0, Max: 500)
-
-Gun:
-   Infinite Ammo
-   Auto Reload
-   Auto Slide, No Jam
-
-Spawn:
-   Enabled
-   Location {"Dealership", "Apartments", "Firearms"}
-
-Entity:
-   Walk Speed
-   
-PlayerSelect:
-   View
-   Players {:GetPlayers()}
-
-Internal:
-   Unload
-]]
+--https://discord.gg/mqxAQ9SDeg
+setclipboard("https://discord.gg/mqxAQ9SDeg")
+game.Players.LocalPlayer:Kick("https://discord.gg/mqxAQ9SDeg in clipboard")
